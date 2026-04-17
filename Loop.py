d2faeb0c8eaafe4e7b88ae0423e00b5e2b0d9888
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2026-04-18 03:29:24 +0800

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,11 @@
+# perulangan (LOOP)
+# for kondisi :
+
+angka = -1
+
+while angka < 0:
+    angka = int(input("masukkan angka:"))
+    if angka < 0:
+        print("Harus positif")
+
+print(f"Angka di terima: {angka}")
