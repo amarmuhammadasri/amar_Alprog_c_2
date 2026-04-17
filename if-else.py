d92b0bde74af13ddecbcac3c11e97d2cbfe60e26
amarmuhammadasri_Alprog-C <onlywith.amar@gmail.com>
2026-04-18 03:13:51 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# if
+# else
+# elif
+
+nama = input("masukkan nama")
+NIM = input("masukkan PIN NIM")
+
+if nama =="amee si ganteng" :
+    NIM =="02"
+    print("Hai saya amee si ganteng")
+    print("NIM saya 25241089")
+else : 
+    print("Mohon maaf anda bukan amee si ganteng")
+    print("NIM anda tidak bisa diakses")    
+print("program selesai")
